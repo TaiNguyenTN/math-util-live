0f8ee9772281c4fc00ffa3dde39423af572af81a
TaiNguyenTN <nguyenquoctai2022nt@gmail.com>
2025-09-07 19:31:25 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -20,6 +20,10 @@ public class MathUtilLive {
         System.out.println("Expected: 0! = 1; actual: " + MathUtil.GetFactorial(0));
         
         //MathUtil.GetFactorial(-5); ném ngoại lệ thật rồi, ai biểu đưa cà chớn
+        
+        //THÊM CODE SAU LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY - CODE LÊN SERVER 7:29PM 7/9/2025
+        System.out.println("Expected: 1! = 1; actual: " + MathUtil.GetFactorial(1));
+        System.out.println("Expected: 3! = 6; actual: " + MathUtil.GetFactorial(3));
     }
     
 }
